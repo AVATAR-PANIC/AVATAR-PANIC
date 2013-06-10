791dc19c5eb73eb1e99c4e5fceb777f9c581a910
Silver0ne <wolfercm@miamioh.edu>
2013-06-10 17:26:11 -0300

    Update GoogleMapsCluster.java
    Returned deleted code.

---
+++ AVATAR working copy/src/sate2012/avatar/android/googlemaps/GoogleMapsCluster.java
@@ -0,0 +1,35 @@
+package sate2012.avatar.android.googlemaps;
+
+import java.util.ArrayList;
+
+public class GoogleMapsCluster {
+
+private ArrayList<MarkerPlus> clusters = new ArrayList<MarkerPlus>();
+private int MAXDISTANCE = 100;
+
+
+/**
+* Default Constructor
+*/
+public GoogleMapsCluster(){
+
+}
+
+public void setClusters(ArrayList<MarkerPlus> clusters){
+this.clusters = clusters;
+}
+
+public ArrayList<MarkerPlus> getClusters(){
+return this.clusters;
+}
+
+public ArrayList<MarkerPlus> generateClusters(int zoomLevel){
+
+for(int i = 0; i < this.clusters.size(); i++){
+System.out.println(this.clusters.get(i).toString());
+}
+
+return this.clusters;
+}
+
+}
