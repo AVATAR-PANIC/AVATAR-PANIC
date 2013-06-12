a95fccb033004007c354117d0011b2ecbd0cdf27
Matthew <matthewtrumpet94@gmail.com>
2013-06-12 09:59:54 -0400

    FTP Stuff is working.

--- AVATAR working copy/src/sate2012/avatar/android/UploadFTP.java
+++ AVATAR working copy/src/sate2012/avatar/android/UploadFTP.java
@@ -23,7 +23,7 @@ public class UploadFTP extends AsyncTask<String, String, String> {
 			ftpClient.connect(InetAddress.getByName("10.0.10.147"));
 			ftpClient.login("Sean", "");
 			Looper.prepare();
-			//ftpClient.changeWorkingDirectory("../../var/www/avatar/Uploaded");
+			ftpClient.changeWorkingDirectory("AVATAR Pictures/");
 			//if (ftpClient.getReplyString().contains("250")) {
 				Handler progressHandler = new Handler();
 				ftpClient
