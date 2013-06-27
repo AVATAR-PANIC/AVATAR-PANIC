a42ebb09dbc5c0aa314ad8dd9464a4c8dc668b4e
Garrett <emrickgarrett@gmail.com>
2013-06-27 09:34:22 -0400

    fix

--- AVATAR working copy/src/sate2012/avatar/android/HandleID.java
+++ AVATAR working copy/src/sate2012/avatar/android/HandleID.java
@@ -107,13 +107,13 @@ public class HandleID extends AsyncTask<Void, Void, Boolean> {
 					}catch(Exception ex){
 						System.err.println("Something went wrong with my script :(");
 						ex.printStackTrace();
-						bool = true;
+						bool = false;
 					}
 				}
 			}catch(Exception ex){
 				System.err.println("Something Wrong with accessing the file");
 				ex.printStackTrace();
-				bool = true;
+				bool = false;
 			}
 			
 			}
